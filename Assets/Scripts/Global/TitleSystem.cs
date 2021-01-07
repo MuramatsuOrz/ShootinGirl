542e341fa8d3ad5411c5870e843b70a77a1aae41
Ryunosuke <matsumura@gs.haselab.net>
2021-01-07 14:06:04 +0900

    タイトル画面の追加，ゲームのスタートと終了を実装

---
+++ Assets/Scripts/Global/TitleSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleSystem : MonoBehaviour
+{
+    //Mainシーンへ遷移する関数
+    public void StartGame() {
+        SceneManager.LoadScene("Main");
+    }
+
+    //アプリケーションを終了する関数
+    public void QuitGame() {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            Application.Quit();
+        #endif
+    }
+
+}
+
