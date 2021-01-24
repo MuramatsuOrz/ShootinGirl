ef1ec70481c6874d74054d17c898110c77fcc3ad
Ryunosuke <matsumura@gs.haselab.net>
2021-01-25 02:22:22 +0900

    敵の9WayShotの追加

---
+++ Assets/Scripts/Shot/DestroyShotParent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class DestroyShotParent : MonoBehaviour
+{
+    //子オブジェクトを保存するリスト
+    [SerializeField]
+    private int childCount;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        childCount = this.transform.childCount;
+        if(childCount == 0) {
+            Destroy(this.gameObject);
+        }
+    }
+}
